02a9b507073ab4687ee933af6094654a65550eb7
Martin <martinmmbote@gmail.com>
2024-04-09 19:35:57 +0300

    first commit

---
+++ backend/config.js
@@ -0,0 +1,3 @@
+export const PORT = 5555;
+
+export const mongoDBURL = 'mongodb+srv://martin:Student.2021@books-store-mern.3sstju7.mongodb.net/Books-Store-MERN?retryWrites=true&w=majority&appName=Books-Store-MERN'
